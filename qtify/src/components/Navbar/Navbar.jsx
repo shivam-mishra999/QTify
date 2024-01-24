fb2743ea8f3cbb8a46848e7f3e71e4ef8b7b9a53
Shivam <mishra.shiva999@gmail.com>
2024-01-24 19:57:04 +0530

    button

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
+import Button from "../Button/Button";
 
 export default function Navbar() {
   return (
@@ -11,6 +12,7 @@ export default function Navbar() {
             <Logo />
         </a>
         <Search placeholder= {"Search a album of your choice"} />
+        <Button>Give Feedback</Button>
       </nav>
     </div>
   )
