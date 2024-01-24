36e9ec578740c66e82401274a231e6dabc426bc4
Shivam <mishra.shiva999@gmail.com>
2024-01-24 19:48:24 +0530

    search bar

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ReactComponent as SearchIcon} from "../../assets/search_icon.svg";
+import {ReactComponent as SearchIcon} from "../../assets/Search_icon.svg";
 import styles from "./Search.module.css";
 
 export default function Search({placeholder}) {
@@ -9,7 +9,7 @@ export default function Search({placeholder}) {
     }
   return (
     <div>
-      <form className={styles.wrapper}>
+      <form className={styles.wrapper} onSubmit={onSubmit}>
         <input className={styles.search} placeholder={placeholder} required />
         <button className={styles.searchButton} type="submit">
             <SearchIcon />
