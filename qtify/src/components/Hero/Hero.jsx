cd6f9cf307189c9a8f87c37f278e16eab1a6afff
Shivam <mishra.shiva999@gmail.com>
2024-01-25 10:01:33 +0530

    hero check

---
+++ qtify/src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <div className={styles.hero}>
+        <div>
+            <h1>100 Thousand Songs, ad-free</h1>
+            <h1>Over thousands podcast episodes</h1>
+        </div>
+        <div>
+            <img src={require("../../assets/vibrating-headphone.png")} width={212} alt="headphones" />
+        </div>
+      
+    </div>
+  )
+}
