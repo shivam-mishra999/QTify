834252add5eac2ad36dfbf64e868e676211274f9
Shivam <mishra.shiva999@gmail.com>
2024-01-31 14:32:43 +0530

    songs section

--- qtify/src/pages/HomePage/HomePage.jsx
+++ qtify/src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import Hero from "../../components/Hero/Hero"
 import { useOutletContext } from 'react-router-dom';
 import Section from "../../components/Section/Section"
 import styles from "./HomePage.module.css";
+import {fetchFilters} from "../../api/api"
 
 export default function HomePage() {
     const {data} = useOutletContext();
@@ -13,6 +14,7 @@ export default function HomePage() {
         <div className={styles.wrapper}>
             <Section title="Top Albums" data={topAlbums} type="album" />
             <Section title="New Albums" data={newAlbums} type="album" />
+            <Section title="songs" data={songs} type="song" filterSource={fetchFilters} />
         </div>
     
     </>
